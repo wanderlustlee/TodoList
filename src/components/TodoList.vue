d9dd2a6f14e5461d6bdfc07d3c3c813e7d7d3bb0
lichao <lichao@bbktel.com>
2019-10-21 14:13:49 +0800

    优化代码 精简代码

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <header>
-      <img src="../assets/logo.png" />
+      <img src="../assets/logo.png"/>
       <div>TodoList</div>
-      
     </header>
 
     <section class="add-section">
@@ -14,22 +13,18 @@
         @keyup.enter="addTodoItem"
         placeholder="添加待办事项"
       />
-
       <a @click="addTodoItem">✓</a>
     </section>
 
     <section>
-
       <!-- 已完成 -->
-      <div v-if="open">
         <ul>
-          <li v-for="(item, index) in todoList" :key="index" v-if="item.done">
-            <div class="view-done">
-
-              <input type="checkbox" @change="changeTodoItemStatus(index,false)" checked="checked" />
+          <li v-for="(item, index) in todos" :key="index" >
+            <div :class="getViewClass(item.done)">
+              <input v-if="item.done" type="checkbox" @change="changeTodoItemStatus(index,false)" checked="checked"/>
+              <input v-else type="checkbox" @change="changeTodoItemStatus(index,true)" />
               <label @dblclick="getEdit(index)">{{item.todoDescription}}</label>
               <a @click="deleteTodoItem(index,false)" href="javascript:void(0)">✕</a>
-              
               <input
                 class="edit"
                 v-todo-focus="item.editing"
@@ -38,47 +33,22 @@
                 @blur="saveEdit(index, $event)"
                 @keydown.enter="saveEdit(index, $event)"
               />
-
             </div>
           </li>
         </ul>
-      </div>
-
-      <!-- 未完成 -->
-      <ul>
-        <li v-for="(item, index) in todoList" :key="index" v-if="!item.done">
-          <div class="view">
-
-            <input type="checkbox" @change="changeTodoItemStatus(index,true)" />
-            <label @dblclick="getEdit(index)">{{item.todoDescription}} </label>
-            <a @click="deleteTodoItem(index,true)" href="javascript:void(0)">✕</a>
-            
-            <textarea
-              class="edit"
-              v-todo-focus="item.editing"
-              v-if="item.editing"
-              :value="item.todoDescription"
-              @blur="saveEdit(index, $event)"
-              @keydown.enter="saveEdit(index, $event)"
-            />
-
-          </div>
-        </li>
-      </ul>
-
       <!-- 统计 -->
       <ul>
         <li class="todo-result-li">
-            <div class="todo-result">
-              <span>未完成：{{todoLength}}/{{this.todoList.length}}</span>
-            </div>
+          <div class="todo-result">
+            <span>未完成：{{todoLength}}/{{this.todoList.length}}</span>
+          </div>
         </li>
       </ul>
     </section>
 
     <footer>
       <span class="switch">
-        <input class="switch-box point" type="checkbox" checked="checked" v-model="open" />显示已完成
+        <input class="switch-box point" type="checkbox" checked="checked" v-model="open"/>显示已完成
       </span>
       <a @click="clearData()" href="javascript:void(0)">清空</a>
     </footer>
@@ -86,144 +56,108 @@
 </template>
 
 <script>
-import * as Utils from "../utils";
+  import * as Utils from '../utils'
 
-export default {
-  name: "Todolist",
+  export default {
+  name: 'Todolist',
 
-  data() {
+  data () {
     return {
-      todoDescription: "", //待办事项描述
+      todoDescription: '', // 待办事项描述
       todoList: [], // 所有待办
-      todoLength: 0, //剩余待办数量
-      open: true
-    };
+      open: true // 是否显示已完成
+    }
   },
 
   computed: {
-    doneList() {
-      console.log("!!")
-      return this.todoList.filter(item => item.done)
+    // 根据是否显示已完成事项动态调整list显示
+    todos() {
+      return this.open ? this.todoList : this.todoList.filter(todo => !todo.done);
     },
-    
-    notDoneList() {
-      console.log("!!")
-      return this.todoList.filter(item => !item.done)
+    // 使用计算属性，就不需要声明属性并维护length的状态
+    todoLength() {
+      return this.todoList.filter(todo => !todo.done).length;
     }
   },
 
   methods: {
     // 如果缓存中有，初始化todoList
-    initTodoList() {
-      const todoStorage = Utils.getItem("todoList");
-      if (todoStorage) {
-        for (const iterator of todoStorage) {
-          if (!iterator.done ) {
-            this.todoLength++;
-          }
-        }
-
-        this.todoList = todoStorage;
-      }
+    initTodoList () {
+      this.todoList = Utils.getItem("todoList", []);
     },
     // 添加todo
-    addTodoItem() {
+    addTodoItem () {
       // 如果内容为空则不添加
-      if(this.todoDescription == "") {
-        return;
+      if (this.todoDescription === '') {
+        return
       }
       let todoItem = {
         todoDescription: this.todoDescription,
         done: false,
         editing: false
-      };
-      // 从localstorage中取
-      var todoStorage = Utils.getItem("todoList");
-      if (todoStorage) {
-        todoStorage.push(todoItem);
-        Utils.setItem("todoList", todoStorage);
-      } else {
-        todoStorage = [];
-        todoStorage.push(todoItem);
-        Utils.setItem("todoList", todoStorage);
       }
+      // 从localstorage中取
+      const todoStorage = Utils.getItem('todoList', [])
+      todoStorage.push(todoItem);
+      Utils.setItem("todoList", todoStorage);
 
-      this.todoList.push(todoItem);
-      this.todoLength++;
-      this.todoDescription = "";
+      this.todoList.push(todoItem)
+      this.todoDescription = ''
     },
     // 删除todo
-    deleteTodoItem(index, done) {
-      if (done) {
-        this.todoLength--;
-      }
-
-      this.todoList.splice(index, 1);
-      Utils.setItem("todoList", this.todoList);
+    deleteTodoItem (index, done) {
+      this.todoList.splice(index, 1)
+      Utils.setItem('todoList', this.todoList)
     },
     //更改状态
-    changeTodoItemStatus(index, done) {
-      if (done) {
-        // let todoItemTemp = {
-        //   todoDescription: this.todoList[index].todoDescription,
-        //   done: true
-        // };
-
-        this.todoLength--;
-        this.todoList[index].done = true;
-        // this.todoList.splice(index, 1);
-        // this.todoList.push(todoItemTemp);
-
-        Utils.setItem("todoList", this.todoList);
-      } else {
-        this.todoLength++;
-        this.todoList[index].done = false;
-        Utils.setItem("todoList", this.todoList);
-      }
+    changeTodoItemStatus (index, done) {
+      this.todoList[index].done = done
+      Utils.setItem('todoList', this.todoList)
     },
     // 清空列表
-    clearData() {
-      localStorage.clear();
-      this.todoList = [];
-      this.todoLength = 0;
+    clearData () {
+      Utils.removeItem('todoList')
+      this.todoList = []
     },
 
     // 唤起编辑
-    getEdit(index) {
-      this.todoList[index].editing = true;
+    getEdit (index) {
+      this.todoList[index].editing = true
     },
 
     // 保存编辑
-    saveEdit(index, event) {
-      this.todoList[index].editing = false;
-      const editValue = event.target.value;
+    saveEdit (index, event) {
+      this.todoList[index].editing = false
+      const editValue = event.target.value
       // 如果编辑之后内容为空，则删除事项
       if (!editValue.length) {
-        this.todoList.splice(index, 1);
+        this.todoList.splice(index, 1)
       } else {
-        this.todoList[index].todoDescription = event.target.value;
-        Utils.setItem("todoList", this.todoList);
+        this.todoList[index].todoDescription = event.target.value
+        Utils.setItem('todoList', this.todoList)
       }
+    },
+
+    getViewClass(done) {
+      return done ? "view-done" : "view";
     }
   },
 
-// 双击编辑待办事项时获取焦点
+  // 双击编辑待办事项时获取焦点
   directives: {
-    "todo-focus": {
+    'todo-focus': {
       inserted: function (el, binding) {
-      
         if (binding.value) {
-          el.focus();
+          el.focus()
         }
-
       }
     }
   },
-// 钩子函数，页面加载完执行
-  mounted() {
-    this.initTodoList();
+  // 钩子函数，页面加载完执行
+  mounted () {
+    this.initTodoList()
   }
-};
+}
 </script>
 
 
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,11 @@
 export function setItem(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
-
-export function getItem(key) {
-    return JSON.parse(localStorage.getItem(key));
+// 需要注意！如果localStorage.getItem(key)是undefined，JSON.parse会报错。
+// 设置默认值，这样就不必每次在使用getItem后都判断存不存在
+export function getItem(key, defaultVal) {
+  const value = localStorage.getItem(key)
+  return value ? JSON.parse(value) : defaultVal
 }
 
 export function removeItem(key) {
